81803d5b010b81223d634307d27c0f60f72cc866
kwlucas <lucaswkyle@gmail.com>
2022-11-24 14:09:36 -0700

    Added directory list

--- client/src/utils/DirectoryContext.js
+++ client/src/utils/DirectoryContext.js
@@ -17,6 +17,8 @@ export default function DirectoryProvider({ children }) {
     return setDirectory(directory);
   };
 
+  const directories = ['About Me', 'Portfolio', 'Contact', 'Resume'];
+
   // The provider component will wrap all other components inside of it that need access to our global state
   return (
     // Dark theme and toggle theme are getting provided to the child components
