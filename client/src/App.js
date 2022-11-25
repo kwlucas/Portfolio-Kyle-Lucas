3d158713eff204faeb8520399e6e865c4520d86e
kwlucas <lucaswkyle@gmail.com>
2022-11-25 12:55:24 -0700

    Wrapped components Using global state

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { DirectoryProvider } from './utils/DirectoryContext';
 import Header from './components/Header';
 import Content from './components/Content';
 import Footer from './components/Footer';
@@ -8,11 +9,11 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
+    <DirectoryProvider>
       <Header />
       <Content />
       <Footer />
-    </>
+    </DirectoryProvider>
   );
 }
 
