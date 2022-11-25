cbc7d6870f88a594a8f5034b16dea435d89c52e1
kwlucas <lucaswkyle@gmail.com>
2022-11-25 11:44:00 -0700

    Removed home from directory array

--- client/src/utils/DirectoryContext.js
+++ client/src/utils/DirectoryContext.js
@@ -17,7 +17,7 @@ export default function DirectoryProvider({ children }) {
     return setDirectory(directory);
   };
 
-  const directories = ['Home', 'About Me', 'Portfolio', 'Contact', 'Resume'];
+  const directories = ['About Me', 'Portfolio', 'Contact', 'Resume'];
 
   // The provider component will wrap all other components inside of it that need access to our global state
   return (
