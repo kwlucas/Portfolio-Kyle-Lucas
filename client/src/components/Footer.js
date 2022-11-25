4d10a350a37323023b728985a4691ff6fc230e54
kwlucas <lucaswkyle@gmail.com>
2022-11-25 12:06:44 -0700

    Set up base footer component

---
+++ client/src/components/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default function Footer() {
+
+  return (
+    <footer>
+        <div>
+            <i>Icon</i>
+            <p>Lable</p>
+        </div>
+        <div>
+            <i>Icon</i>
+            <p>Lable</p>
+        </div>
+        <div>
+            <i>Icon</i>
+            <p>Lable</p>
+        </div>
+    </footer>
+  );
+}
