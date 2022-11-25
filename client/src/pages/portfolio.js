0a96c97d705455e2dfa0af3ac782ea431a071308
kwlucas <lucaswkyle@gmail.com>
2022-11-25 14:59:11 -0700

    Set up protfolio page with placeholder projects

--- client/src/pages/portfolio.js
+++ client/src/pages/portfolio.js
@@ -1,9 +1,41 @@
 import React from 'react';
 
+import Project from '../components/Project';
+
+const projects = [
+    {
+        name: 'Project 1',
+        description: 'Awsome description of project.',
+        repo: 'linkToRepo.com',
+        deployment: 'linkToDeployment.com',
+        imagePath: 'path/to/image'
+    },
+    {
+        name: 'Project 2',
+        description: 'Awsome description of project.',
+        repo: 'linkToRepo.com',
+        deployment: 'linkToDeployment.com',
+        imagePath: 'path/to/image'
+    },
+    {
+        name: 'Project 3',
+        description: 'Awsome description of project.',
+        repo: 'linkToRepo.com',
+        deployment: 'linkToDeployment.com',
+        imagePath: 'path/to/image'
+    },
+]
+
 export default function Portfolio() {
 
-  return (
-    <main>
-    </main>
-  );
+    return (
+        <main>
+            <h2>Portfolio</h2>
+            <div className='cardContainer'>
+                {projects.map((project, index) => (
+                    <Project key={index} projectData={project} />
+                ))}
+            </div>
+        </main>
+    );
 }
