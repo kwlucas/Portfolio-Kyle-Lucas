7db06aaeb1ed121768d67312adf9cbfd3691f0fa
kwlucas <lucaswkyle@gmail.com>
2022-11-25 11:56:34 -0700

    Formatted navigation

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -5,22 +5,15 @@ import { useDirectory } from '../utils/DirectoryContext';
 // Make our ThemeComponent the default export from this file
 export default function Navigation() {
   // Pluck values from our ThemeContext by invoking our useDirectory hook
-  const { currentDirectory, setCurrentDirectory, directories } = useDirectory();
+  const { currentDirectory, changeDirectory, directories } = useDirectory();
 
   return (
     <>
-      <navbar>
-        {directories.map((directory) => (
-          <div key={directory} className={currentDirectory == directory ? 'tab selected' : 'tab'} onClick={() => setCurrentDirectory(`${directory}`)}>
-            {directory}
-          </div>
-        ))}
-      </navbar>
-      {(currentDirectory == 'About Me') ? (
-        <div></div>
-      ) : (
-        <div></div>
-      )}
+      {directories.map((directory) => (
+        <div key={directory} className={currentDirectory == directory ? 'tab selected' : 'tab'} onClick={() => changeDirectory(`${directory}`)}>
+          {directory}
+        </div>
+      ))}
     </>
   );
 }
