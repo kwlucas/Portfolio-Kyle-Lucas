8db900a84fbd7810d16b85d9d9663c6a4866ea14
kwlucas <lucaswkyle@gmail.com>
2022-11-25 12:55:07 -0700

    Adjusted and formatted

--- client/src/utils/DirectoryContext.js
+++ client/src/utils/DirectoryContext.js
@@ -1,28 +1,33 @@
 import React, { useState, useContext, createContext } from 'react';
 
-// Create our theme context using React.CreateContext()
+// Create the directory context using React.CreateContext()
 const DirectoryContext = createContext();
 
 // Create a custom hook that allows easy access to our DirectoryContext values
 export const useDirectory = () => useContext(DirectoryContext);
 
-// Creating our theme provider. Accepts an argument of "props", here we plucking off the "children" object.
-export const  DirectoryProvider = ({ children }) => {
-  // Creating our state
+// Creating the directory provider. Accepts an argument of "props", here we plucking off the "children" object.
+export const DirectoryProvider = ({ children }) => {
+  // Creating state
   const [currentDirectory, setDirectory] = useState('Home');
 
   // Method to update our state
   const changeDirectory = (directory) => {
+    if (!directory) {
+      return;
+    }
     console.log('inside directory change');
-    return setDirectory(directory);
+    setDirectory(directory);
   };
 
   const directories = ['About', 'Portfolio', 'Contact', 'Resume'];
 
   // The provider component will wrap all other components inside of it that need access to our global state
   return (
-    <DirectoryContext.Provider value={{ currentDirectory, changeDirectory, directories }}>
+    <DirectoryContext.Provider
+      value={{ currentDirectory, changeDirectory, directories }}
+    >
       {children}
     </DirectoryContext.Provider>
   );
-}
+};
