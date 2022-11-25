a76899a3fa0d6aecedf61db494349189e17ab235
kwlucas <lucaswkyle@gmail.com>
2022-11-25 15:30:22 -0700

    Set up resume page

--- client/src/pages/resume.js
+++ client/src/pages/resume.js
@@ -4,6 +4,11 @@ export default function Resume() {
 
   return (
     <main>
+        <h2>Resume</h2>
+        <div>
+            <p><span>Download</span> my resume!</p>
+            <iframe src='https://kwlucas.github.io/Resume-KyleLucas/' title='My resume!'></iframe>
+        </div>
     </main>
   );
 }
