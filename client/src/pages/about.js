5f5f0a011ef17d73b209c5e774de0a7e549ad0d0
kwlucas <lucaswkyle@gmail.com>
2022-11-25 14:07:53 -0700

    Basic layout

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -2,8 +2,13 @@ import React from 'react';
 
 export default function About() {
 
-  return (
-    <main>
-    </main>
-  );
+    return (
+        <main>
+            <h2>About Me</h2>
+            <div className='container'>
+                <p>Text goes here</p>
+                <img alt='Kyle Lucas'></img>
+            </div>
+        </main>
+    );
 }
