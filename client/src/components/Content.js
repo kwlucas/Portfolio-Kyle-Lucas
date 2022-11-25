d465e0d791d4e3e2732c51c64401e1d21b00b1e9
kwlucas <lucaswkyle@gmail.com>
2022-11-25 12:41:32 -0700

    Corrected import paths

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,10 +1,10 @@
 import React from 'react';
-import { useDirectory } from './utils/DirectoryContext';
+import { useDirectory } from '../utils/DirectoryContext';
 
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
+import About from '../pages/About';
+import Contact from '../pages/Contact';
+import Portfolio from '../pages/Portfolio';
+import Resume from '../pages/Resume';
 
 export default function Content() {
 
