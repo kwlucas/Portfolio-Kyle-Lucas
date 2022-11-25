d7ec54bcee36ba420fcc3d974a89dca7a394caac
kwlucas <lucaswkyle@gmail.com>
2022-11-25 11:53:50 -0700

    Added homepage navigation to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import { useDirectory } from '../utils/DirectoryContext';
 import Navigation from './navigation';
 
-export default function Header(directory) {
+export default function Header() {
+
+  const { currentDirectory, changeDirectory } = useDirectory();
+
+  function nameClick() {
+    if(currentDirectory != 'Home'){
+      changeDirectory('Home');
+    }
+  }
 
   return (
-      <header className={directory == 'Home'? 'banner': 'navbar'}>
+      <header className={currentDirectory == 'Home'? 'banner': 'navbar'}>
         <div >
-          <h1>Kyle Lucas</h1>
+          <h1 onClick={nameClick}>Kyle Lucas</h1>
           <div>Web Developer</div>
         </div>
         <Navigation />
