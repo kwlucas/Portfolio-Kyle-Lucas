c40154b90bd229792da2d1433e55eaa8bc33c421
kwlucas <lucaswkyle@gmail.com>
2022-11-25 12:40:02 -0700

    Transfered directory use from app into content

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,14 @@
+import React from 'react';
 import './App.css';
-import { useDirectory } from './utils/DirectoryContext';
 import Header from './components/Header';
 import Footer from './components/Footer';
-
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Portfolio from './pages/Portfolio';
-import Resume from './pages/Resume';
 
 
 
 function App() {
-
-  const { currentDirectory } = useDirectory();
-
-  const renderPage = () => {
-    if (currentDirectory === 'About') {
-      return <About />;
-    }
-    if (currentDirectory === 'Portfolio') {
-      return <Portfolio />;
-    }
-    if (currentDirectory === 'Contact') {
-      return <Contact />;
-    }
-    if (currentDirectory === 'Resume') {
-      return <Resume />;
-    }
-  };
-
-
   return (
     <>
       <Header />
-      {renderPage()}
       <Footer />
     </>
   );
---
+++ client/src/components/Content.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useDirectory } from './utils/DirectoryContext';
+
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+
+export default function Content() {
+
+    const { currentDirectory } = useDirectory();
+
+    const renderPage = () => {
+        if (currentDirectory === 'About') {
+            return <About />;
+        }
+        if (currentDirectory === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentDirectory === 'Contact') {
+            return <Contact />;
+        }
+        if (currentDirectory === 'Resume') {
+            return <Resume />;
+        }
+    };
+
+    return (
+        <>
+        {renderPage()}
+        </>
+    );
+}
