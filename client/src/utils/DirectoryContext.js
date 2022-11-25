ddfb172cca40b99800353bb104b3517c5b87b6ec
kwlucas <lucaswkyle@gmail.com>
2022-11-25 12:36:22 -0700

    Adjusted exports

--- client/src/utils/DirectoryContext.js
+++ client/src/utils/DirectoryContext.js
@@ -1,13 +1,13 @@
 import React, { useState, useContext, createContext } from 'react';
 
 // Create our theme context using React.CreateContext()
-export const DirectoryContext = createContext();
+const DirectoryContext = createContext();
 
 // Create a custom hook that allows easy access to our DirectoryContext values
 export const useDirectory = () => useContext(DirectoryContext);
 
 // Creating our theme provider. Accepts an argument of "props", here we plucking off the "children" object.
-export default function DirectoryProvider({ children }) {
+export const  DirectoryProvider = ({ children }) => {
   // Creating our state
   const [currentDirectory, setDirectory] = useState('Home');
 
@@ -21,7 +21,6 @@ export default function DirectoryProvider({ children }) {
 
   // The provider component will wrap all other components inside of it that need access to our global state
   return (
-    // Dark theme and toggle theme are getting provided to the child components
     <DirectoryContext.Provider value={{ currentDirectory, changeDirectory, directories }}>
       {children}
     </DirectoryContext.Provider>
