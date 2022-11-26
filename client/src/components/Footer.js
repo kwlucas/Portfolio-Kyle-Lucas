51645e96cd4dc31ffb1a2bf346cefd801f9a8db3
kwlucas <lucaswkyle@gmail.com>
2022-11-26 12:19:54 -0700

    Formatted footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,21 +1,20 @@
 import React from 'react';
 
 export default function Footer() {
-
-  return (
-    <footer>
-        <div>
-            <i>Icon</i>
-            <p>Lable</p>
-        </div>
-        <div>
-            <i>Icon</i>
-            <p>Lable</p>
-        </div>
-        <div>
-            <i>Icon</i>
-            <p>Lable</p>
-        </div>
-    </footer>
-  );
+    return (
+        <footer>
+            <div>
+                <i>Icon</i>
+                <p>Lable</p>
+            </div>
+            <div>
+                <i>Icon</i>
+                <p>Lable</p>
+            </div>
+            <div>
+                <i>Icon</i>
+                <p>Lable</p>
+            </div>
+        </footer>
+    );
 }
