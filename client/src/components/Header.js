55eb356c9903d37d761813285dc1122752ce9531
kwlucas <lucaswkyle@gmail.com>
2022-11-26 12:20:33 -0700

    Corrected color loop

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -47,7 +47,7 @@ export default function Header() {
         from: index
       })
     }).add({
-      backgroundColor: colors[waveCount % (colors.length - 1)],
+      backgroundColor: colors[waveCount % (colors.length)],
     })
     setWaveCount(waveCount + 1);
   }
