6b2f3feaed8fa179b5255869fa62726a8ce54bcc
kwlucas <lucaswkyle@gmail.com>
2022-11-26 12:20:08 -0700

    Adjusted animation

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useDirectory } from '../utils/DirectoryContext';
 import Navigation from './Navigation';
+import anime from 'animejs';
 
 export default function Header() {
 
@@ -9,6 +10,14 @@ export default function Header() {
     width: document.body.clientWidth,
     height: document.body.clientHeight
   });
+  const [waveCount, setWaveCount] = useState(0);
+
+
+  const colors = [
+    "rgb(42, 51, 84, 1.0)",
+    "rgb(42, 51, 84, 0.0)",
+    "rgb(35, 51, 71, 0.0)"
+  ]
 
   const handleResize = () => {
     setSize({
@@ -25,37 +34,50 @@ export default function Header() {
 
   window.addEventListener('resize', handleResize);
 
-  const handleMouseEnter = (event) => {
-    console.log('Mouse Enter Tile');
-    //event.target.style.color = "orange";
+  const handleMouseEnter = async (event) => {
+    //console.log('Mouse Enter Tile');
+    const index = [...event.target.parentElement.children].indexOf(event.target);
+    let columns = Math.floor(size.width / 50);
+    let rows = Math.floor(size.height / 50);
+    console.log(waveCount % (colors.length));
+    anime.timeline({
+      targets: '.tile',
+      delay: anime.stagger(50, {
+        grid: [columns, rows],
+        from: index
+      })
+    }).add({
+      backgroundColor: colors[waveCount % (colors.length - 1)],
+    })
+    setWaveCount(waveCount + 1);
   }
 
   function calculateTiles() {
-    let columns = Math.floor(size.width / 30);
-    let rows = Math.floor(size.height / 30);
+    let columns = Math.floor(size.width / 50);
+    let rows = Math.floor(size.height / 50);
     document.documentElement.style.setProperty('--columns', columns);
     document.documentElement.style.setProperty('--rows', rows);
     return rows * columns;
   }
 
-  const createTiles = Array.from({length: calculateTiles()}, (_, index) => {
-    return <div key={index} className='tile' onMouseEnter={handleMouseEnter}></div>;
+  const createTiles = Array.from({ length: calculateTiles() }, (_, index) => {
+    return <div key={index} className='tile' onClick={handleMouseEnter}></div>;
   });
 
   function nameClick() {
-    if(currentDirectory !== 'Home'){
+    if (currentDirectory !== 'Home') {
       changeDirectory('Home');
     }
   }
 
   return (
-      <header className={currentDirectory === 'Home'? 'banner': 'navbar'}>
-        {currentDirectory === 'Home'? <div id='bannerGrid'>{createTiles}</div>:<></>}
-        <div id='nameDisplay'>
-          <h1 onClick={nameClick}>Kyle Lucas</h1>
-          <div>Web Developer</div>
-        </div>
-        {currentDirectory === 'Home'? <nav><Navigation /></nav>: <Navigation />}
-      </header>
+    <header className={currentDirectory === 'Home' ? 'banner' : 'navbar'}>
+      {currentDirectory === 'Home' ? <div id='bannerGrid'>{createTiles}</div> : <></>}
+      <div id='nameDisplay'>
+        <h1 onClick={nameClick}>Kyle Lucas</h1>
+        <div>Web Developer</div>
+      </div>
+      {currentDirectory === 'Home' ? <nav><Navigation /></nav> : <Navigation />}
+    </header>
   );
 }
