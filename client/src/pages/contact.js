5de46fad582a9f5cc3b5f07df60e7319e47af53e
kwlucas <lucaswkyle@gmail.com>
2022-11-28 10:19:48 -0700

    Added ids to form inputs

--- client/src/pages/contact.js
+++ client/src/pages/contact.js
@@ -4,12 +4,14 @@ export default function Contact() {
 
   return (
     <main>
-        <h2>Contact</h2>
-        <form>
-            <input placeholder='Name'></input>
-            <input placeholder='Email'></input>
-            <input placeholder='Message'></input>
-        </form>
+      <h2>Contact</h2>
+      <form>
+        <fieldset>
+          <input id='nameInput' placeholder='Name'></input>
+          <input id='emailInput' placeholder='Email'></input>
+        </fieldset>
+        <textarea id='messageInput' placeholder='Message'></textarea>
+      </form>
     </main>
   );
 }
