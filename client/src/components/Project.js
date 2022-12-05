65344e312c91d7b396f22acc913c3ecae3b32ebc
kwlucas <lucaswkyle@gmail.com>
2022-12-05 11:56:41 -0700

    Rearranged card layout to allow for hover effect

--- client/src/components/Project.js
+++ client/src/components/Project.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Project(props) {
     const { name, description, repo, deployment, imagePath } = props.projectData
     return (
-        <div className='card' onMouseMove={props.eventHandler}>
+        <div className='card'>
             <div className='content'>
                 <div className='title'>{name}</div>
                 <div className='body' >{description}</div>
