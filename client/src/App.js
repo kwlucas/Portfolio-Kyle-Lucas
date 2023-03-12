f5cd0f53f647a7ffb15f7e4afa3d2e68137baea4
kwlucas <lucaswkyle@gmail.com>
2023-03-11 20:13:14 -0700

    Set up react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
 import './App.css';
 import { DirectoryProvider } from './utils/DirectoryContext';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/Header';
-import Content from './components/Content';
+// import Content from './components/Content';
 import Footer from './components/Footer';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
 
 
 function App() {
   return (
-    <DirectoryProvider>
-      <Header />
-      <Content />
-      <Footer />
-    </DirectoryProvider>
+    <Router>
+      <DirectoryProvider>
+        <Header />
+        <Routes>
+          <Route path='/' element={<></>} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+          <Route path='/portfolio' element={<Portfolio />} />
+          <Route path='/resume' element={<Resume />} />
+        </Routes>
+        <Footer />
+      </DirectoryProvider>
+    </Router>
   );
 }
 
