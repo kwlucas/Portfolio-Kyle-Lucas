5b7da4075c10299571811751825b46b57134ac05
kwlucas <lucaswkyle@gmail.com>
2023-03-11 20:13:55 -0700

    Switched from directory to router

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,17 +1,21 @@
 import React from 'react';
 // Import our custom hook.
 import { useDirectory } from '../utils/DirectoryContext';
+import { NavLink } from "react-router-dom";
 
 // Make our ThemeComponent the default export from this file
 export default function Navigation() {
   // Pluck values from our ThemeContext by invoking our useDirectory hook
-  const { currentDirectory, changeDirectory, directories } = useDirectory();
+  const { directories } = useDirectory();
 
+  // <div key={directory} className={currentDirectory === directory ? 'tab selected' : 'tab'} onClick={() => changeDirectory(`${directory}`)}>
+  //         {directory}
+  //       </div>
   return (
     <>
-      {directories.map((directory) => (
-        <div key={directory} className={currentDirectory === directory ? 'tab selected' : 'tab'} onClick={() => changeDirectory(`${directory}`)}>
-          {directory}
+      {directories.map((directory, index) => (
+        <div key={index} className='tab'>
+          <NavLink to={`/${directory}`}>{directory}</NavLink>
         </div>
       ))}
     </>
