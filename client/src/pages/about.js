2bcd1db0b142f3858e919496995ef1aabbfc7bfa
Kyle <lucaswkyle@gmail.com>
2025-01-18 15:30:41 -0700

    Adjusted about page

--- client/src/pages/about.js
+++ client/src/pages/about.js
@@ -5,11 +5,24 @@ export default function About() {
 
     return (
         <main>
-            <h2>About Me</h2>
-            <h3>My Story</h3>
             <div className='about container'>
                 <img src={aboutImage} alt='Kyle Lucas'></img>
-                <p>I am a young programmer who has a real passion for the industry. I have always been rather objective and logical and prior to programming professionally I wrote code as a hobby for many years.<br></br>My journey with programming goes all the way back to when I was in 5th grade. I had begun dabbling in the basics of Ruby to make games using a software called "RPG Maker". I worked my way into Python from there, and everything seemed to grow from that. Python, C++, C#, Bash; I found myself stockpiling skills for pretty much nothing but hobbies. Then, near the end of highschool Covid came around and we were all in quarantine. That turned me onto social media which I had never been very active in. It was there I heard that I could create chat bots using Javascript, so I taught myself that and started making bots on Discord, a text based social media platform.</p>
+                <section>
+                    <div>
+                        <div>
+                            <h2>Kyle Lucas</h2>
+                        </div>
+                        <div>
+                            <h3>Developer</h3>
+                            <div>Coding</div>
+                            <div>Design</div>
+                            <div>Passion</div>
+                        </div>
+                    </div>
+                    <div>
+                        <p>I am a young programmer who has a real passion for the industry. I have always been rather objective and logical and prior to programming professionally I wrote code as a hobby for many years.<br></br>My journey with programming goes all the way back to when I was in 5th grade. I had begun dabbling in the basics of Ruby to make games using a software called "RPG Maker". I worked my way into Python from there, and everything seemed to grow from that. Python, C++, C#, Bash; I found myself stockpiling skills for pretty much nothing but hobbies. Then, near the end of highschool Covid came around and we were all in quarantine. That turned me onto social media which I had never been very active in. It was there I heard that I could create chat bots using Javascript, so I taught myself that and started making bots on Discord, a text based social media platform.</p>
+                    </div>
+                </section>
             </div>
             <p>Not long after I started creating bots I started doing freelance bot development. The most difficult part of that was getting clients, I had to do a lot of networking and getting myself out there, but it soon became pretty successful, at least for a high-schooler who didn't have to pay rent. After highschool I headed off to college but quickly realized that it was not for me. I looked into alternative education tracks and I soon found myself in a coding boot camp program at the University of Denver. It was extremely fast paced, but I was very interested in the topic and was determined to learn and I excelled. Learning advanced coding in an official, flushed out capacity was a game changer as I had, up until that point been a completely self-taught programmer. Once I had graduated I began doing freelance web development with a focus on front-end. During this time I fell into the niche of creating static portfolio sites for artists. These sites were primarily developed using the React.js framework which I had taken a liking to. One time after delivering one of these React portfolios the client asked if I was looking for work as she knew a friend who had a company that was looking for React developers. A few interviews after that and I was brought on as an intern at Verkrea, a company in Germany focused on developing a platform to better connect artists and other freelancers with their clients.</p>
             <p>The internship at Verkrea has been going phenomenally. The company is in the midst of converting an HTML and python (flask) based site to the MERN stack. During this process I primarily worked on bringing the HTML and JavaScript front-end into React. As we near the end of the conversion process I have begun to work on creating brand new React pages based on wireframes directly from the designers.</p>
